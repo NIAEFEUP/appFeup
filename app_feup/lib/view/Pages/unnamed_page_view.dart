3ceeaa31775e8f845f0bd199ce8c474c3f20717c
gui1612 <gui1612@sapo.pt>
2021-04-13 23:29:54 +0100

    docs: documented view

--- app_feup/lib/view/Pages/unnamed_page_view.dart
+++ app_feup/lib/view/Pages/unnamed_page_view.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 
 import 'general_page_view.dart';
 
-abstract class UnnamedPageView extends GeneralPageViewState {
+/// Generic class implementation for the user's personal page view
+abstract class UnnamedPageView extends GeneralPageViewState{
   @override
   getScaffold(BuildContext context, Widget body) {
     return Scaffold(
